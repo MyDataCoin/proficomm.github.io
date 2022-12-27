5c9a60cc2344797a89cd0eb421a6f2b269c07484
Bermet <85883631+BermetBakyt@users.noreply.github.com>
2022-12-27 17:47:51 +0600

    changed

--- js/i18n.js
+++ js/i18n.js
@@ -23,8 +23,8 @@ i18n
       escapeValue: false
     },
   backend: {
-      loadPath: "/<https://bermetbakyt.github.io>/locales/{{ru}}/{{translation}}.json",
-      loadPath: "/<https://bermetbakyt.github.io>/locales/{{en}}/{{translation}}.json"
+      loadPath: "/./locales/en/translation.json",
+      loadPath: "/./locales/ru/translation.json"
   }
 })
 
