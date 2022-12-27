bed9c784eeee138e2a7fa33eae809fcaedb84b38
Bermet <85883631+BermetBakyt@users.noreply.github.com>
2022-12-27 17:43:52 +0600

    added again

--- js/i18n.js
+++ js/i18n.js
@@ -23,8 +23,8 @@ i18n
       escapeValue: false
     },
   backend: {
-      loadPath: "/<bermetbakyt.github.io/proficommunications.github.io>/locales/{{ru}}/{{translation}}.json",
-      loadPath: "/<bermetbakyt.github.io/proficommunications.github.io>/locales/{{en}}/{{translation}}.json"
+      loadPath: "/<https://bermetbakyt.github.io>/locales/{{ru}}/{{translation}}.json",
+      loadPath: "/<https://bermetbakyt.github.io>/locales/{{en}}/{{translation}}.json"
   }
 })
 
